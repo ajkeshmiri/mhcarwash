f67b77123e53b32a3836bfe5987e70918e8020e9
Armon Keshmiri <ajkeshmiri@gmail.com>
2023-05-22 21:20:04 -0700

    adding styling

--- app.js
+++ app.js
@@ -3,14 +3,14 @@ const answer = document.querySelectorAll('#answer');
 
 
 collapsible[0].addEventListener('click', function () {
-    console.log('hello');
     answer[0].classList.toggle('hide');
+    answer[0].classList.toggle('show');
 })
 collapsible[1].addEventListener('click', function () {
-    console.log('hello');
     answer[1].classList.toggle('hide');
+    answer[1].classList.toggle('show');
 })
 collapsible[2].addEventListener('click', function () {
-    console.log('hello');
     answer[2].classList.toggle('hide');
+    answer[2].classList.toggle('show');
 })
